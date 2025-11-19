224bbd1d0c32f4d7f1f7c47b34ed273632f26422
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-11-19 23:00:23 +0100

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -13,7 +13,8 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
+    
 from django.contrib import admin
 from django.urls import path ,include
 from api.views import BookList
@@ -23,4 +24,22 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('api/', include('my_app.urls')),  # connect app routes
+]    
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Token endpoint
+    path('api/token/', obtain_auth_token),
+
+    # API app URLs
+    path('api/', include('api.urls')),
 ]
+
+
+
