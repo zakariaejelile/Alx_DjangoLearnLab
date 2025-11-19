4881b908c4c281f6a9e801cd5af7684c69b2149d
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-11-19 22:59:59 +0100

    Add token authentication endpoint to URLs

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, BookList  # if you still use BookList
+from .views import BookViewSet, BookList
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='books_all')
@@ -11,4 +12,9 @@ urlpatterns = [
 
     # Router CRUD routes
     path('', include(router.urls)),
+
+    # Token endpoint (POST: username, password)
+    path('get-token/', obtain_auth_token, name='get-token'),
 ]
+
+
