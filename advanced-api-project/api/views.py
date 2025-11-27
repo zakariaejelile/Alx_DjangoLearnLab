378aa99ab09790f398b5c8c23c4b8a99f075d5f8
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-11-27 23:49:54 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -114,4 +114,32 @@ class BookDeleteView(mixins.DestroyModelMixin,
         return self.destroy(request, *args, **kwargs)
 
 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book_detail.html"
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = "__all__"
+    template_name = "book_form.html"
+    success_url = reverse_lazy("book_list")
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = "__all__"
+    template_name = "book_form.html"
+    success_url = reverse_lazy("book_list")
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = "book_confirm_delete.html"
+    success_url = reverse_lazy("book_list")
 
