5c64309b8c368c9e02f1b4eb55acf1c54183c796
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-11-27 23:46:31 +0100

    Refactor book views with mixins and permissions
    Refactored book views to use mixins for CRUD operations and added public access for listing and retrieving books.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,18 +1,6 @@
-'''
-
-from rest_framework import generics
-from .models import Author, Book
-from .serializers import AuthorSerializer, BookSerializer
-
-class AuthorListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-
-class BookListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
 '''
+
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
@@ -42,7 +30,88 @@ class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly] # Only authenticated users can modify
+'''
+from rest_framework import generics, permissions, mixins
+from .models import Book
+from .serializers import BookSerializer
 
+
+class BookListView(mixins.ListModelMixin,
+                   generics.GenericAPIView):
+    """
+    ListView:
+    - Lists all books (GET)
+    - Public access allowed
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # Read access to everyone
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class BookDetailView(mixins.RetrieveModelMixin,
+                     generics.GenericAPIView):
+    """
+    DetailView:
+    - Retrieves a single book by ID (GET)
+    - Public access allowed
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+class BookCreateView(mixins.CreateModelMixin,
+                     generics.GenericAPIView):
+    """
+    CreateView:
+    - Create a new book (POST)
+    - Only authenticated users can create
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class BookUpdateView(mixins.UpdateModelMixin,
+                     generics.GenericAPIView):
+    """
+    UpdateView:
+    - Update an existing book (PUT/PATCH)
+    - Authenticated users only
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+
+class BookDeleteView(mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
+    """
+    DeleteView:
+    - Delete a book (DELETE)
+    - Authenticated users only
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
 
