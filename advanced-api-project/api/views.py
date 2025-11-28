4c65750c9fca027d07d5af3fd5d824dea85ab0f9
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-11-28 23:57:47 +0100

    add from django_filters import rest_framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -33,6 +33,7 @@ class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 '''
 from rest_framework import generics, permissions, filters ,mixins
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework   as django_filters
 from .models import Book
 from .serializers import BookSerializer
 
