f69a6b34ba656f5a88301ddc061f7ce91de6ec97
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-03 23:13:59 +0100

    Delete django_blog directory

--- django_blog/blog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def home(request):
-    posts = Post.objects.all() # Fetch all blog posts
-    return render(request, 'blog/home.html', {'posts': posts})
