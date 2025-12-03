61710da1bd8bf4c42eb6374a03180974931113d5
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-04 00:08:32 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
 
     # Blog post CRUD URLs
     path('posts/', PostListView.as_view(), name='post-list'),                  # List all posts
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),          # Create a new post
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),     # View post details
-    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),# Edit a post
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),# Delete a post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),          # Create a new post
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),     # View post details
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),# Edit a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),# Delete a post
 ]
