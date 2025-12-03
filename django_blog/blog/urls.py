f35ab0eb95ff00e310175f2cc8ae8cf5f36798e3
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-03 23:59:14 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,3 +1,4 @@
+"""
 from django.urls import path
 from . import views
 
@@ -16,5 +17,31 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),  # Profile page
 ]
 
+"""
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views  # Make sure to import your views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Homepage URL
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+]
+
+
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
+
+urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
 
 
