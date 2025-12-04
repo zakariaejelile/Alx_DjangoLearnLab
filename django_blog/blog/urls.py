9c9e723a37b2e9b0bc5e59a9ce13086c70c02ffb
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-04 22:48:30 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 from .views import (
     PostListView, PostDetailView, PostCreateView,
-    PostUpdateView, PostDeleteView,
+    PostUpdateView, PostDeleteView, TagPostListView,SearchResultsView,
     CommentUpdateView, CommentDeleteView, add_comment ,CommentCreateView
 )
 
@@ -29,5 +29,10 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path("post/<int:pk>/comment/", add_comment, name="add_comment"),
+
+    #Tag URLs
+    path('tag/<str:tag_name>/', TagPostListView.as_view(), name='tag-posts'),
+    # search URL
+    path('search/', SearchResultsView.as_view(), name='search-results'),
 
 ]
