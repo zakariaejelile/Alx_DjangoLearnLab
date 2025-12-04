4dc2ba95a12fb99ca1917915a35fa2d501082e63
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-04 22:17:19 +0100

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -160,3 +160,23 @@ def add_comment(request, pk):
             Comment.objects.create(post=post, text=text)
 
     return redirect('post_detail', pk=pk)
+
+from django.views.generic import CreateView
+from django.shortcuts import get_object_or_404
+from .models import Comment, Post
+from .forms import CommentForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        form.instance.author = self.request.user
+        form.instance.post = post
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
