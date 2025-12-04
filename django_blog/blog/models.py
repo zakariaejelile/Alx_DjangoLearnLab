4948f3b1faf97a05cd95aec3d73c666d27c3ecd1
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-04 23:08:08 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -36,10 +36,19 @@ class Comment(models.Model):
     
 
 # Tag model for categorizing posts
+from django.utils.text import slugify
+
 class Tag(models.Model):
     name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=30, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+
     
 
