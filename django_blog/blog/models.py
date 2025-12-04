bfe22381d8e0f29386dda3325f3ae48f850770e8
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-04 22:47:35 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,6 +8,8 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts') # Many-to-many relationship with Tag model
+
 
     def __str__(self):
         return self.title
@@ -29,4 +31,14 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         # after creating/editing a comment, go back to the post detail
-        return reverse('post-detail', kwargs={'pk': self.post.pk})
+         return reverse('post-detail', kwargs={'pk': self.post.pk})
+    
+
+# Tag model for categorizing posts
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
