8dd3b2b1dd889bc6d7e291870368df74e412e31e
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-04 23:00:39 +0100

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -193,11 +193,24 @@ class TagPostListView(ListView):
         tag = self.kwargs.get("tag_name")
         return Post.objects.filter(tags__name=tag)
 
+from django.db.models import Q
+from django.views.generic import ListView
+from .models import Post
+
 class SearchResultsView(ListView):
     model = Post
     template_name = "blog/search_results.html"
+    context_object_name = "posts"
 
     def get_queryset(self):
         query = self.request.GET.get("q")
-        return Post.objects.filter(title__icontains=query)
+        if query:
+            return Post.objects.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(tags__name__icontains=query)
+            ).distinct()
+        return Post.objects.none()
+
+
 
