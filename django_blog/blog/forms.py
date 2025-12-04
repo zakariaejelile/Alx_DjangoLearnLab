9e0ec4b37da0fdb3b8e3e72db0d60ff3961fd24f
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-04 22:08:08 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -18,3 +18,14 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), label='')
+
+    class Meta:
+        model = Comment
+        fields = ['content']
