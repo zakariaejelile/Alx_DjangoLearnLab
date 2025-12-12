0ced17697b955df4265f99f794e3be025a3f5e8c
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-13 00:29:58 +0100

    Delete social_media_api directory

--- social_media_api/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
-
-
-urlpatterns = [
-path('register/', RegisterView.as_view(), name='register'),
-path('login/', LoginView.as_view(), name='login'),
-path('profile/', ProfileView.as_view(), name='profile'),
-]
