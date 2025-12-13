764bf3b9dc745fccae305793d09d42e3f442aa33
jelile zakariae <93086084+zakariaejelile@users.noreply.github.com>
2025-12-13 22:58:56 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,7 +10,7 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
 path('', include(router.urls)),
-path('feed/', FeedView.as_view(), name='user-feed'),
+path('feed/', FeedView.as_view(), name='feed'),
 ]
 
 
